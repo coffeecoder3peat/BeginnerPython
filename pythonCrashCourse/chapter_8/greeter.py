68c2f107b44bb1995a44dfe646c024856bdb4905
coffeecoder3peat <philip.c.wilson@outlook.com>
2019-05-22 10:54:43 -0700

    updates for new chapters

---
+++ pythonCrashCourse/chapter_8/greeter.py
@@ -0,0 +1,10 @@
+def greet_user(username):
+    """Display a simple Greeting"""
+    print(f"Hello {username.title()}!")
+
+greet_user("Chase")
+
+def favorite_book(title):
+    print(f"One of my favorite books is {title.title()}.")
+
+favorite_book('Alice in wonderland')
