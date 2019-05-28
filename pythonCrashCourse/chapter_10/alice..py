8a4f052f26cbde0d200282704554be034ceda188
coffeecoder3peat <philip.c.wilson@outlook.com>
2019-05-28 01:08:44 -0700

    update for the rest of the normal chapters

--- pythonCrashCourse/chapter_10/alice..py
+++ pythonCrashCourse/chapter_10/alice..py
@@ -1,7 +1,15 @@
-filename = 'alice.txt'
+def count_words(filename):
+    """Count the approximate number of words in a file."""
+    file_path = "pythonCrashCourse\chapter_10"
+    try:
+        with open(f"{file_path}\{filename}", encoding='utf-8') as f:
+            contents = f.read()
+    except:
+        pass
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {filename} has about {num_words} words.")
 
-try:
-    with open(filename, encoding='utf-8') as f:
-        contents = f.read()
-except:
-    print(f"Sorry, the file {filename} does not exist.")
+filename = 'alice.txt'
+count_words(filename)
